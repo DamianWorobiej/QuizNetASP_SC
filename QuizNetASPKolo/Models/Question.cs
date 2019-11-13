349332f6b0fdc3c4c7106197368647f19fc0e9d6
DamianWorobiej <damian.worobiej@gmail.com>
2019-11-13 18:42:50 +0100

    [add] Question model

---
+++ QuizNetASPKolo/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizNetASPKolo.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public Answer[] Answers { get; set; }
+
+        public int CorrectAnswerIndex { get; set; }
+    }
+}
