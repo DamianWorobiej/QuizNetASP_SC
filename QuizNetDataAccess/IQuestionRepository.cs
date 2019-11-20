91a302cb2f3f080adcdfbc28d630b418bd44bcbd
DamianWorobiej <damian.worobiej@gmail.com>
2019-11-20 19:18:29 +0100

    [add] question repository interface

---
+++ QuizNetDataAccess/IQuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using QuizNetDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNetDataAccess
+{
+    public interface IQuestionRepository
+    {
+        IEnumerable<Question> GetAll();
+        Question GetById(int id);
+        void Add(Question question);
+        void Update(Question question);
+        void Delete(int questionId);
+    }
+}
