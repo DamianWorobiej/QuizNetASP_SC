09b22451c9e9888312d48d82773c0b1a2cc04090
DamianWorobiej <damian.worobiej@gmail.com>
2019-11-20 19:18:46 +0100

    [add] base in memory question repository class

---
+++ QuizNetDataAccess/InMemoryQuestionRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuizNetDataAccess.Models;
+
+namespace QuizNetDataAccess
+{
+    public class InMemoryQuestionRepository : IQuestionRepository
+    {
+        public void Add(Question question)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Question> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Question GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Question question)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
