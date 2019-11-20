b914b869c2bdc6c6251e5e5c06a7da19a84b6218
DamianWorobiej <damian.worobiej@gmail.com>
2019-11-20 19:41:07 +0100

    [update] question controller using DataAccess project

--- QuizNetASPKolo/Controllers/QuestionController.cs
+++ QuizNetASPKolo/Controllers/QuestionController.cs
@@ -5,98 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using QuizNetASPKolo.Models;
+using QuizNetDataAccess;
+using QuizNetDataAccess.Models;
 
 namespace QuizNetASPKolo.Controllers
 {
     public class QuestionController : Controller
     {
-        private readonly static List<Question> _questions = new List<Question>()
-        {
-            new Question()
-            {
-                Id = 1,
-                Text = "Jakie jest hasło admina?",
-                Answers = new Answer[]
-                {
-                    new Answer()
-                    {
-                        Id = 1,
-                        QuestionId = 1,
-                        Text = "qwerty"
-                    },
-                    new Answer()
-                    {
-                        Id = 2,
-                        QuestionId = 1,
-                        Text = "admin"
-                    },
-                    new Answer()
-                    {
-                        Id = 3,
-                        QuestionId = 1,
-                        Text = "haslo"
-                    },
-                    new Answer()
-                    {
-                        Id = 4,
-                        QuestionId = 1,
-                        Text = "password"
-                    },
-                },
-                CorrectAnswerIndex = 1
-            },
-            new Question()
-            {
-                Id = 2,
-                Text = "Jakie jest najlepszy język programowania?",
-                Answers = new Answer[]
-                {
-                    new Answer()
-                    {
-                        Id = 5,
-                        QuestionId = 2,
-                        Text = "HTML"
-                    },
-                    new Answer()
-                    {
-                        Id = 6,
-                        QuestionId = 2,
-                        Text = "Java"
-                    },
-                    new Answer()
-                    {
-                        Id = 7,
-                        QuestionId = 2,
-                        Text = "C#"
-                    },
-                    new Answer()
-                    {
-                        Id = 8,
-                        QuestionId = 2,
-                        Text = "JavaScript"
-                    },
-                },
-                CorrectAnswerIndex = 2
-            },
-        };
+        private readonly IQuestionRepository _questionRepository;
 
+        public QuestionController(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
         public IActionResult GetAll()
         {
-            var questions = _questions;
+            var questions = _questionRepository.GetAll();
             return View(questions);
         }
 
         public IActionResult Get(int id)
         {
-            var question = _questions.FirstOrDefault(x => x.Id == id);
+            var question = _questionRepository.GetById(id);
             return View(question);
         }
 
         public IActionResult Delete(int id)
         {
-            var question = _questions.FirstOrDefault(x => x.Id == id);
-            _questions.Remove(question);
+            _questionRepository.Delete(id);
             return RedirectToAction("GetAll");
         }
 
@@ -109,17 +45,17 @@ namespace QuizNetASPKolo.Controllers
         [HttpPost]
         public IActionResult Create(Question question)
         {
-            var lastQuestionId = _questions.Last().Id;
+            var lastQuestionId = _questionRepository.GetAll().Last().Id;
             question.Id = lastQuestionId + 1;
 
-            var lastAnswerId = _questions.LastOrDefault().Answers.LastOrDefault().Id;
+            var lastAnswerId = _questionRepository.GetAll().LastOrDefault().Answers.LastOrDefault().Id;
             for (int i = 0; i < question.Answers.Length; i++)
             {
                 question.Answers[i].Id = lastAnswerId + i + 1;
                 question.Answers[i].QuestionId = question.Id;
             }
 
-            _questions.Add(question);
+            _questionRepository.Add(question);
 
             return RedirectToAction("Get", routeValues: new { Id = question.Id });
         }
