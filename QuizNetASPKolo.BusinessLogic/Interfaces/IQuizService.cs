3eba079865651af83c889fbc190b65afa0781ba8
DamianWorobiej <damian.worobiej@gmail.com>
2019-11-27 18:38:33 +0100

    [add] IQuizService

---
+++ QuizNetASPKolo.BusinessLogic/Interfaces/IQuizService.cs
@@ -0,0 +1,12 @@
+﻿using QuizNetDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNetASPKolo.BusinessLogic
+{
+    public interface IQuizService
+    {
+        List<Question> GenerateQuiz();
+    }
+}
