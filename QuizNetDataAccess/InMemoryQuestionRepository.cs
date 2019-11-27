8ae599efb517212677652afb4a24b7249e0d2c03
DamianWorobiej <damian.worobiej@gmail.com>
2019-11-27 18:39:21 +0100

    [hotfix] more questions in repository

--- QuizNetDataAccess/InMemoryQuestionRepository.cs
+++ QuizNetDataAccess/InMemoryQuestionRepository.cs
@@ -8,12 +8,13 @@ namespace QuizNetDataAccess
 {
     public class InMemoryQuestionRepository : IQuestionRepository
     {
-        private readonly static List<Question> _questions = new List<Question>()
+        private static readonly List<Question> _questions = new List<Question>()
         {
             new Question()
             {
                 Id = 1,
                 Text = "Jakie jest hasło admina?",
+                CorrectAnswerIndex = 1,
                 Answers = new Answer[]
                 {
                     new Answer()
@@ -21,32 +22,36 @@ namespace QuizNetDataAccess
                         Id = 1,
                         QuestionId = 1,
                         Text = "qwerty"
+
                     },
                     new Answer()
                     {
                         Id = 2,
                         QuestionId = 1,
                         Text = "admin"
+
                     },
                     new Answer()
                     {
                         Id = 3,
                         QuestionId = 1,
-                        Text = "haslo"
+                        Text = "hasło"
+
                     },
                     new Answer()
                     {
                         Id = 4,
                         QuestionId = 1,
                         Text = "password"
+
                     },
-                },
-                CorrectAnswerIndex = 1
+                }
             },
             new Question()
             {
                 Id = 2,
-                Text = "Jakie jest najlepszy język programowania?",
+                Text = "Jaki jest najlepszy język programowania?",
+                CorrectAnswerIndex = 2,
                 Answers = new Answer[]
                 {
                     new Answer()
@@ -54,28 +59,130 @@ namespace QuizNetDataAccess
                         Id = 5,
                         QuestionId = 2,
                         Text = "HTML"
+
                     },
                     new Answer()
                     {
                         Id = 6,
                         QuestionId = 2,
                         Text = "Java"
+
                     },
                     new Answer()
                     {
                         Id = 7,
                         QuestionId = 2,
                         Text = "C#"
+
                     },
                     new Answer()
                     {
                         Id = 8,
                         QuestionId = 2,
                         Text = "JavaScript"
+
                     },
-                },
-                CorrectAnswerIndex = 2
+                }
             },
+            new Question()
+            {
+                Id = 3,
+                Text = "Jakiej metody HTTP używamy aby pobrać dane z serwera?",
+                CorrectAnswerIndex = 3,
+                Answers = new Answer[]
+                {
+                    new Answer()
+                    {
+                        Id = 9,
+                        QuestionId = 3,
+                        Text = "POST"
+                    },
+                    new Answer()
+                    {
+                        Id = 10,
+                        QuestionId = 3,
+                        Text = "PUT"
+                    },
+                    new Answer()
+                    {
+                        Id = 11,
+                        QuestionId = 3,
+                        Text = "DELETE"
+                    },
+                    new Answer()
+                    {
+                        Id = 12,
+                        QuestionId = 3,
+                        Text = "GET"
+                    },
+                }
+            },
+            new Question()
+            {
+                Id = 4,
+                Text = "Na jakim systemie operacyjnym możemy uruchomić aplikację napisaną w .NET Core?",
+                CorrectAnswerIndex = 3,
+                Answers = new Answer[]
+                {
+                    new Answer()
+                    {
+                        Id = 13,
+                        QuestionId = 4,
+                        Text = "Windows"
+                    },
+                    new Answer()
+                    {
+                        Id = 14,
+                        QuestionId = 4,
+                        Text = "Linux"
+                    },
+                    new Answer()
+                    {
+                        Id = 15,
+                        QuestionId = 4,
+                        Text = "Mac OS"
+                    },
+                    new Answer()
+                    {
+                        Id = 16,
+                        QuestionId = 4,
+                        Text = "Wszystkie wymienione"
+                    },
+                }
+            },
+            new Question()
+            {
+                Id = 5,
+                Text = "Które zdanie najlepiej opisuje kontroler?",
+                CorrectAnswerIndex = 2,
+                Answers = new Answer[]
+                {
+                    new Answer()
+                    {
+                        Id = 17,
+                        QuestionId = 5,
+                        Text = "Wykonuje operacje przeszukiwania i modyfikacji bazy danych"
+                    },
+                    new Answer()
+                    {
+                        Id = 18,
+                        QuestionId = 5,
+                        Text = "Realizuje algorytmy sztucznej inteligencji"
+                    },
+                    new Answer()
+                    {
+                        Id = 19,
+                        QuestionId = 5,
+                        Text = "Odpowiada na zapytania wysyłane przez klienta"
+                    },
+                    new Answer()
+                    {
+                        Id = 20,
+                        QuestionId = 5,
+                        Text = "Tworzy instancje obiektów zainicjalizowane w kontenerze DI"
+                    },
+                }
+            }
         };
 
         public void Add(Question question)
