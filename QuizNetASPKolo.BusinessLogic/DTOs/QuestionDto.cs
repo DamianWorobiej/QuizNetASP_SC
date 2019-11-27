6d79b80896d43d92cf4e2f5e991328cd2a9d9df1
DamianWorobiej <damian.worobiej@gmail.com>
2019-11-27 18:54:37 +0100

    [add] QuestionDto

---
+++ QuizNetASPKolo.BusinessLogic/DTOs/QuestionDto.cs
@@ -0,0 +1,18 @@
+﻿using QuizNetDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNetASPKolo.BusinessLogic.DTOs
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public Answer[] Answers { get; set; }
+
+        public int CorrectAnswerIndex { get; set; }
+    }
+}
