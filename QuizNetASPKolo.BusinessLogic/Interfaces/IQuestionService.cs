608903d403e952b5a80f1a59129830d4d12d3b70
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-01 17:11:07 +0100

    [add] IQuestionService

---
+++ QuizNetASPKolo.BusinessLogic/Interfaces/IQuestionService.cs
@@ -0,0 +1,16 @@
+﻿using QuizNetASPKolo.BusinessLogic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNetASPKolo.BusinessLogic.Interfaces
+{
+    interface IQuestionService
+    {
+        IEnumerable<QuestionDto> GetAll();
+        QuestionDto GetById(int id);
+        void Add(QuestionDto question);
+        void Update(QuestionDto question);
+        void Delete(int questionId);
+    }
+}
