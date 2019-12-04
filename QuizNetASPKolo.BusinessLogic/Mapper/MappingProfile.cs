8e9a2ef4f65df517fde00dd47f44f8dd5778137b
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-04 17:40:58 +0100

    [add] AutoMapper MappingProfile class

---
+++ QuizNetASPKolo.BusinessLogic/Mapper/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using QuizNetASPKolo.BusinessLogic.DTOs;
+using QuizNetDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNetASPKolo.BusinessLogic.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Answer, AnswerDto>();
+            CreateMap<Question, QuestionDto>();
+
+            CreateMap<AnswerDto, Answer>();
+            CreateMap<QuestionDto, Question>();
+        }
+    }
+}
