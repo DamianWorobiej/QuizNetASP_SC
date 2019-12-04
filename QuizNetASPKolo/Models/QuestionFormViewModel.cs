e0b98f549128e95caaaf8dcd8ba2218813c28341
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-04 17:47:04 +0100

    [update] removed deprecated methods from question form view model

--- QuizNetASPKolo/Models/QuestionFormViewModel.cs
+++ QuizNetASPKolo/Models/QuestionFormViewModel.cs
@@ -22,38 +22,5 @@ namespace QuizNetASPKolo.Models
         {
             Question = new QuestionDto();
         }
-
-        public QuestionFormViewModel(Question question)
-        {
-            QuestionDto parsedQuestion = new QuestionDto()
-            {
-                Id = question.Id,
-                CorrectAnswerIndex = question.CorrectAnswerIndex,
-                Text = question.Text,
-                Answers = question.Answers.Select(y => new AnswerDto()
-                {
-                    Id = y.Id,
-                    Text = y.Text,
-                    QuestionId = y.QuestionId
-                }).ToArray()
-            };
-            Question = parsedQuestion;
-        }
-
-        public Question GetQuestion()
-        {
-            return new Question()
-            {
-                Id = Question.Id,
-                CorrectAnswerIndex = Question.CorrectAnswerIndex,
-                Text = Question.Text,
-                Answers = Question.Answers.Select(y => new Answer()
-                {
-                    Id = y.Id,
-                    Text = y.Text,
-                    QuestionId = y.QuestionId
-                }).ToArray()
-            };
-        }
     }
 }
