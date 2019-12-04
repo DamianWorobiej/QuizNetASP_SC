b9c0f7d4120ee1e71dde748f75d8ee874ae17ab3
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-04 19:45:00 +0100

    [add] QuizSummaryViewModel

---
+++ QuizNetASPKolo/Models/QuizSummaryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using QuizNetASPKolo.BusinessLogic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizNetASPKolo.Models
+{
+    public class QuizSummaryViewModel
+    {
+        public List<QuestionDto> Questions { get; set; }
+        public int[] UserAnswersIndexes { get; set; }
+        public int CorrectAnswers { get; set; }
+        public double PercentageCorrect
+        {
+            get
+            {
+                return 100 * (double)CorrectAnswers / Questions.Count();
+            }
+        }
+
+        public string ClassNamesForAnswer(int questionIndex, int answerIndex)
+        {
+            if (Questions[questionIndex].CorrectAnswerIndex == answerIndex)
+            {
+                return "list-group-item-success";
+            }
+            
+            if (UserAnswersIndexes[questionIndex] == answerIndex)
+            {
+                return "list-group-item-danger";
+            }
+
+            return string.Empty;
+        }
+    }
+}
