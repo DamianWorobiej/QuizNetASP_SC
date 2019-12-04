be431e42d12c63c8066665b11504691d54ca9afd
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-04 17:45:43 +0100

    [update] implemented AutoMapper in quiz service

--- QuizNetASPKolo.BusinessLogic/QuizService.cs
+++ QuizNetASPKolo.BusinessLogic/QuizService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using AutoMapper;
 using QuizNetASPKolo.BusinessLogic.DTOs;
 using QuizNetDataAccess;
 using QuizNetDataAccess.Models;
@@ -11,10 +12,12 @@ namespace QuizNetASPKolo.BusinessLogic
     public class QuizService : IQuizService
     {
         private readonly IQuestionRepository _questionRepository;
+        private readonly IMapper _mapper;
 
-        public QuizService(IQuestionRepository questionRepository)
+        public QuizService(IQuestionRepository questionRepository, IMapper mapper)
         {
             _questionRepository = questionRepository;
+            _mapper = mapper;
         }
 
         public List<QuestionDto> GenerateQuiz()
@@ -22,19 +25,9 @@ namespace QuizNetASPKolo.BusinessLogic
             List<Question> questions = _questionRepository.GetAll().ToList();
             var randomQuestions = questions.OrderBy(x => Guid.NewGuid()).Take(2).ToList();
 
-            List<QuestionDto> randomQuestionDtos = randomQuestions.Select(x => new QuestionDto()
-            {
-                Id = x.Id,
-                CorrectAnswerIndex = x.CorrectAnswerIndex,
-                Text = x.Text,
-                Answers = x.Answers.Select(y => new AnswerDto()
-                {
-                    Id = y.Id,
-                    Text = y.Text,
-                    QuestionId = y.QuestionId
-                }).ToArray()
-            }).ToList();
-            return randomQuestionDtos;
+            List<QuestionDto> randomQuestionsDto = _mapper.Map<List<QuestionDto>>(randomQuestions);
+
+            return randomQuestionsDto;
         }
     }
 }
