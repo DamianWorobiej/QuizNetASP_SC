b1ac1d5edca9bb1a7a3147c141e288f8d48c31be
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-09 19:56:03 +0100

    [update] cleaned up question dto

--- QuizNetASPKolo.BusinessLogic/DTOs/QuestionDto.cs
+++ QuizNetASPKolo.BusinessLogic/DTOs/QuestionDto.cs
@@ -22,37 +22,5 @@ namespace QuizNetASPKolo.BusinessLogic.DTOs
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
-
-        public static QuestionDto FromQuestion(Question question)
-        {
-            return new QuestionDto()
-            {
-                Id = question.Id,
-                CorrectAnswerIndex = question.CorrectAnswerIndex,
-                Text = question.Text,
-                Answers = question.Answers.Select(y => new AnswerDto()
-                {
-                    Id = y.Id,
-                    Text = y.Text,
-                    QuestionId = y.QuestionId
-                }).ToArray()
-            };
-        }
-
-        public Question ConvertToQuestion()
-        {
-            return new Question()
-            {
-                Id = Id,
-                CorrectAnswerIndex = CorrectAnswerIndex,
-                Text = Text,
-                Answers = Answers.Select(y => new Answer()
-                {
-                    Id = y.Id,
-                    Text = y.Text,
-                    QuestionId = y.QuestionId
-                }).ToArray()
-            };
-        }
     }
 }
