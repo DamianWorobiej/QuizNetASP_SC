50e0766569df75edf2cb15686cb209924da4b573
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-11 19:15:13 +0100

    [add] EFQuestionRepository

---
+++ QuizNetDataAccess/EfQuestionRepository.cs
@@ -0,0 +1,56 @@
+﻿using QuizNetDataAccess;
+using QuizNetDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuizNetASPKolo.DataAccess
+{
+    public class EFQuestionRepository : IQuestionRepository
+    {
+        private readonly EFDbContext _dbContext;
+
+        public EFQuestionRepository(EFDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Question question)
+        {
+            _dbContext.Questions.Add(question);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(int questionId)
+        {
+            var questionToDelete = _dbContext.Questions.SingleOrDefault(x => x.Id == questionId);
+            _dbContext.Questions.Remove(questionToDelete);
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Question> GetAll()
+        {
+            return _dbContext.Questions.AsEnumerable();
+        }
+
+        public Question GetById(int id)
+        {
+            return _dbContext.Questions.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Update(Question question)
+        {
+            var questionToUpdate = _dbContext.Questions.SingleOrDefault(x => x.Id == question.Id);
+            questionToUpdate.Text = question.Text;
+
+            for (int i = 0; i < questionToUpdate.Answers.Count; i++)
+            {
+                questionToUpdate.Answers[i].IsCorrect = question.Answers[i].IsCorrect;
+                question.Answers[i].Text = question.Answers[i].Text;
+            }
+
+            _dbContext.SaveChanges();
+        }
+    }
+}
