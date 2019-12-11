dec9b11c8bf9f9c01d1205c1cd8f3a6279a84803
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-11 18:52:11 +0100

    [add] EFDbContext class

---
+++ QuizNetDataAccess/EFDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizNetDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNetASPKolo.DataAccess
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+
+        public EFDbContext(DbContextOptions<EFDbContext> options) : base(options)
+        {
+        }
+    }
+}
