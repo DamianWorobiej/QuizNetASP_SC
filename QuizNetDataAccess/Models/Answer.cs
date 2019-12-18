836f091ca384b0f677773c6fd8c12360440bbddb
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-18 18:28:10 +0100

    [update] added Required to texts

--- QuizNetDataAccess/Models/Answer.cs
+++ QuizNetDataAccess/Models/Answer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuizNetDataAccess.Models
 {
@@ -9,6 +10,7 @@ namespace QuizNetDataAccess.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Text { get; set; }
 
         public int QuestionId { get; set; }
