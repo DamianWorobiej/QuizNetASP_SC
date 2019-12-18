de8ab91cf566c5285ba84d309fe162c916365f1d
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-18 18:30:02 +0100

    [add] RequiredTexts migration

--- QuizNetDataAccess/Migrations/20191218172937_RequiredTexts.Designer.cs
+++ QuizNetDataAccess/Migrations/20191218172937_RequiredTexts.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizNetASPKolo.DataAccess;
 
 namespace QuizNetASPKolo.DataAccess.Migrations
 {
     [DbContext(typeof(EFDbContext))]
-    partial class EFDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191218172937_RequiredTexts")]
+    partial class RequiredTexts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
