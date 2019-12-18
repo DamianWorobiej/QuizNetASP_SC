6a539171b4d8146aadb689fa9e6243e4da3e67ea
DamianWorobiej <damian.worobiej@gmail.com>
2019-12-18 18:56:54 +0100

    [add] QuestionMetadataDto

---
+++ QuizNetASPKolo.BusinessLogic/DTOs/QuestionsMetadataDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNetASPKolo.BusinessLogic.DTOs
+{
+    public class QuestionsMetadataDto
+    {
+        public int QuestionsCount { get; set; }
+
+        public DateTime OldestQuestionDate { get; set; }
+
+        public DateTime NewestQuestionDate { get; set; }
+    }
+}
