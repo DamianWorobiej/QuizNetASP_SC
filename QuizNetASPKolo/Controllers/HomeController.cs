45cb2221c93136fbb1b24331295900bbf48744c7
DamianWorobiej <damian.worobiej@gmail.com>
2020-01-15 19:37:49 +0100

    [update] playing with caching

--- QuizNetASPKolo/Controllers/HomeController.cs
+++ QuizNetASPKolo/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using QuizNetDataAccess.Models;
 
@@ -12,15 +13,40 @@ namespace QuizNetASPKolo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _cache;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMemoryCache cache)
         {
             _logger = logger;
+            _cache = cache;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Cache1()
+        {
+            if (!_cache.TryGetValue("Current time", out DateTime result))
+            {
+                result = DateTime.Now;
+                _cache.Set("Current time", result, TimeSpan.FromSeconds(5));
+            }
+
+            return View("Cache", result);
+        }
+
+        public IActionResult Cache2()
+        {
+            var cacheItem = _cache.GetOrCreate("Current time", entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromSeconds(3);
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(7);
+                return DateTime.Now;
+            });
+
+            return View("Cache", cacheItem);
         }
 
         public IActionResult Privacy()
